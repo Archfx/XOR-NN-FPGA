ff6211c307569100005548e04cf4502e0977ce00
archfx <cybx@live.com>
2019-10-02 17:01:37 +0530

    Initial commit

---
+++ XOR-NN.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:23:05 10/02/2019 
+// Design Name: 
+// Module Name:    XOR-NN 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module XOR_NN(
+	input [16:0] in,
+	output reg [16:0] out
+	
+    );
+
+
+endmodule
